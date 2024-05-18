6d7f6ce28ff17c6428816e2b3705876e15fa4054
Stark-Industries7 <apooravmukherjee@gmail.com>
2024-05-18 16:31:09 +0530

    Update in Function

--- Cryptioner1.py
+++ Cryptioner1.py
@@ -26,7 +26,7 @@ def write_text_file(file_path, content):
     with open(file_path, 'w') as file:
         file.write(content)
 
- def read_word_file(file_path):
+def read_word_file(file_path):
     doc = Document(file_path)
     return '\n'.join([para.text for para in doc.paragraphs])
 
